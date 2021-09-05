38798744d055bbde245635ee26a3ad5aa585d9c9
Kandomere <assasin21rus@gmail.com>
2021-09-05 18:30:02 +0300

    основы работают

---
+++ main.py
@@ -0,0 +1,78 @@
+from aiogram import executor, Bot, types, Dispatcher
+from config import TOKEN
+
+import os
+from datetime import datetime
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputFile
+from PIL import Image, ImageDraw, ImageFont
+import random
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+text_file = os.getcwd() + '\\' + 't.txt'
+images_dir = os.getcwd() + '\\' + 'images'
+create_dir = os.getcwd() + '\\' + 'createimages'
+d = datetime.now()
+today = datetime.today()
+date_p = f'{today.strftime(f"%Y-%m-%d_%H;%M_")}'
+
+btn_rep = InlineKeyboardButton('Да, переслать', callback_data='rep')
+inline_kb = InlineKeyboardMarkup().add(btn_rep)
+
+
+def photo_name(date_p, user_id):
+    return date_p + str(user_id) + '.jpg'
+
+
+def write_text(path_image):
+    with open('t.txt', 'r', encoding="utf-8") as f:
+        s = f.read().split('\n')
+    text = random.choice(s)
+
+    myimage = Image.open(path_image)
+    font = ImageFont.truetype("arial.ttf", 36)
+    draw = ImageDraw.Draw(myimage)
+    width_image, height_image = myimage.size
+
+    width_text, height_text = draw.textsize(text, font=font)
+
+    draw.text(((width_image - width_text) * 0.5, (height_image / 1.1)),
+              text,
+              font=font,
+              fill=(255, 255, 255, 255))
+
+    myimage.save(create_dir + '\\' + path_image.split('\\')[-1])
+    return create_dir + '\\' + path_image.split('\\')[-1]
+
+
+# font_file
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.answer('Отправь мне картинку я напишу туда текст')
+
+
+@dp.message_handler(content_types=['photo'])
+async def handle_docs_photo(message):
+    path_image = f'{images_dir}\\{photo_name(date_p, message.from_user.id)}'
+    await message.photo[-1].download(path_image)
+    # await message.answer(message)
+
+    # -1001277471725
+    p = write_text(path_image)
+
+    photo = InputFile(p)
+    await bot.send_photo(chat_id=message.chat.id, photo=photo)
+    await message.answer('Переслать в другую группу?', reply_markup=inline_kb)
+
+    @dp.callback_query_handler(lambda c: c.data == 'rep')
+    async def callback_inline(callback_query: types.CallbackQuery):
+        await bot.answer_callback_query(callback_query.id)
+        # await bot.send_photo(chat_id=-1001277471725, photo=photo)
+        photo = InputFile(create_dir + '\\' + path_image.split('\\')[-1])
+        await bot.send_photo(chat_id=-1001277471725, photo=photo)
+        # await bot.send_message(chat_id=-1001277471725, text=message)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
